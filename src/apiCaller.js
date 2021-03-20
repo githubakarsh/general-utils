1a8cc4d8a1d005211bb498b73a000f98f6023814
Akarsh Saraff <saraffakarsh@gmail.com>
2021-03-20 23:41:43 +0000

    api caller utility

---
+++ src/apiCaller.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+async function serviceApiRequest(serviceRequest, successManager, failureManager) {
+    const { url, method, headers, body, mode, credentials } = serviceRequest;
+    if (!url || !method) {
+        return;
+    }
+
+    try {
+        const requestData = {
+            method: method,
+            headers: { "Content-Type": "application/json" },
+        };
+
+        if (!_.isEmpty(body) && (method === "POST" || method === "PUT")) {
+            requestData.body = JSON.stringify(body);
+        }
+
+        if (credentials) {
+            requestData.credentials = credentials;
+        }
+        const response = await fetch(url, requestData);
+        const responseData = await response.json();
+
+        if (response.status === 200) {
+            console.log(responseData);
+        } else {
+            console.log("Service Error");
+        }
+    } catch (error) {
+        // throw some error
+        console.log("some error");
+    }
+
+}
+
+export default serviceApiRequest;
